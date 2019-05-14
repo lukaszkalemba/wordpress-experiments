ab7898462ffa872871681309159111b622d039d2
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-14 12:58:00 +0200

    Update gulpfile

--- wp-content/themes/first_theme/front-page.php
+++ wp-content/themes/first_theme/front-page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-  <h1>site title</h1>
+<h1>site title</h1>
 
-    <article>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum id, porro officiis asperiores, ipsam velit dolorum, perspiciatis voluptatum dolorem ut est sed facere placeat suscipit expedita enim corrupti similique. Iusto consequatur soluta id aliquam! Corporis incidunt necessitatibus aperiam ipsam eos est, cum repellendus doloribus porro quisquam animi, numquam laborum. Voluptates labore ad deserunt voluptatibus itaque accusantium ratione, corrupti repudiandae, sint est, assumenda velit minima similique at repellendus. Dicta quasi illo error officia ex natus nam tenetur! Officiis, minus cum? Explicabo mollitia ex sint odit, molestias, temporibus necessitatibus totam aut assumenda doloremque deserunt, atque similique est? Iusto voluptatibus consequuntur culpa repellat.
-    </article>
+<article>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum id, porro officiis asperiores, ipsam velit dolorum, perspiciatis voluptatum dolorem ut est sed facere placeat suscipit expedita enim corrupti similique. Iusto consequatur soluta id aliquam! Corporis incidunt necessitatibus aperiam ipsam eos est, cum repellendus doloribus porro quisquam animi, numquam laborum. Voluptates labore ad deserunt voluptatibus itaque accusantium ratione, corrupti repudiandae, sint est, assumenda velit minima similique at repellendus. Dicta quasi illo error officia ex natus nam tenetur! Officiis, minus cum? Explicabo mollitia ex sint odit, molestias, temporibus necessitatibus totam aut assumenda doloremque deserunt, atque similique est? Iusto voluptatibus consequuntur culpa repellat.
+</article>
 
 <?php get_footer(); ?>
--- wp-content/themes/first_theme/gulpfile.js
+++ wp-content/themes/first_theme/gulpfile.js
@@ -12,7 +12,8 @@ gulp.task('scss', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(concat('main.min.css'))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
@@ -25,16 +26,9 @@ gulp.task('js', function() {
     ])
     .pipe(concat('index.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
 });
-
-gulp.task(
-  'watch',
-  gulp.series(['scss', 'js'], function() {
-    gulp.watch('./src/scss/**/*.scss', gulp.series(['scss']));
-    gulp.watch('./src/js/**/*.js', gulp.series(['js']));
-  })
-);
 
 gulp.task('connect-sync', function() {
   connect.server({}, function() {
@@ -42,10 +36,19 @@ gulp.task('connect-sync', function() {
       proxy: 'localhost/wordpress'
     });
   });
-
-  gulp.watch('**/*.php').on('change', function() {
-    browserSync.reload();
-  });
 });
+
+gulp.task(
+  'watch',
+  gulp.series(['scss', 'js', 'connect-sync'], function() {
+    browserSync.init({
+      server: ''
+    });
+
+    gulp.watch('./src/scss/**/*.scss', gulp.series(['scss']));
+    gulp.watch('./src/js/**/*.js', gulp.series(['js']));
+    gulp.watch('**/*.php').on('change', browserSync.reload);
+  })
+);
 
 gulp.task('default', gulp.series(['scss', 'js', 'connect-sync', 'watch']));
