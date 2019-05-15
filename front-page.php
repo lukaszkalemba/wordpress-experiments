6de19d9dd5083190cbd001fd44beb1714ce62bb0
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-15 13:19:14 +0200

    Add some custom fields to front page using acf

--- front-page.php
+++ front-page.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
 <h1><?php the_title(); ?></h1>
+<h2><?php the_field('podtytul_dodany_wtyczka'); ?></h2>
+<p><?php the_field('opis_dodany_wtyczka'); ?></p>
+<img src="<?php the_field('zdjecie_dodane_wtyczka'); ?>" alt="">
 
 
 <?php while ( have_posts() ) : the_post(); ?>
