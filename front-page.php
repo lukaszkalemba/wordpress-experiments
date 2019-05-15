6a5503504de7749de9a4348b34ce7501d38908aa
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-15 09:24:55 +0200

    Add dynamic titles and content

--- front-page.php
+++ front-page.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
-<h1>site title</h1>
+<h1><?php the_title(); ?></h1>
 
-<article>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum id, porro officiis asperiores, ipsam velit dolorum, perspiciatis voluptatum dolorem ut est sed facere placeat suscipit expedita enim corrupti similique. Iusto consequatur soluta id aliquam! Corporis incidunt necessitatibus aperiam ipsam eos est, cum repellendus doloribus porro quisquam animi, numquam laborum. Voluptates labore ad deserunt voluptatibus itaque accusantium ratione, corrupti repudiandae, sint est, assumenda velit minima similique at repellendus. Dicta quasi illo error officia ex natus nam tenetur! Officiis, minus cum? Explicabo mollitia ex sint odit, molestias, temporibus necessitatibus totam aut assumenda doloremque deserunt, atque similique est? Iusto voluptatibus consequuntur culpa repellat.
-</article>
+
+<?php while ( have_posts() ) : the_post(); ?>
+  <?php the_content(); ?>
+
+<?php
+  endwhile;
+  wp_reset_query();
+?>
 
 <?php get_footer(); ?>
--- page-contact.php
+++ page-contact.php
@@ -6,6 +6,14 @@
 
 get_header(); ?>
 
-<h1>Kontakt</h1>
+<h1><?php the_title(); ?></h1>
+
+<?php while ( have_posts() ) : the_post(); ?>
+  <?php the_content(); ?>
+
+<?php
+  endwhile;
+  wp_reset_query();
+?>
 
 <?php get_footer(); ?>
