e57649711b842b291790208bbb055eff4c6298dc
Lukasz Kalemba <lukasz.kalemba1@gmail.com>
2019-05-17 10:18:22 +0200

    Add services pages and create time and employee fields

---
+++ page-flexible.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+  Template Name: Flexible
+*/
+
+get_header(); ?>
+
+<div class="container-fluid px-5 my-5">
+
+  <h1><?php the_title(); ?></h1>
+
+  <?php while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+
+  <?php
+    endwhile;
+    wp_reset_query();
+  ?>
+
+</div>
+
+
+<?php
+    if( have_rows('uslugi') ):
+      ?>
+
+<?php
+      while ( have_rows('uslugi') ) : the_row();
+      if( get_row_layout() == 'wiersz1' ):
+        ?>
+
+<div class="container-fluid px-5 my-5">
+  <div class="row">
+    <div class="col">
+      <?php the_sub_field('tekst'); ?>
+    </div>
+
+    <div class="col">
+      <?php $image_url = get_sub_field('zdjecie')['url']; ?>
+      <img class="services__image" src="<?php echo $image_url ?>" alt="">
+    </div>
+  </div>
+
+</div>
+
+<?php
+    elseif( get_row_layout() == 'wiersz2' ):
+      ?>
+
+<div class="container-fluid px-5 my-5">
+  <div class="row">
+    <div class="col">
+      <?php $image_url1 = get_sub_field('zdjecie2')['url']; ?>
+      <img class="services__image" src="<?php echo $image_url1 ?>" alt="">
+    </div>
+
+    <div class="col">
+      <?php the_sub_field('tekst2'); ?>
+    </div>
+  </div>
+
+</div>
+
+<?php
+    elseif( get_row_layout() == 'wiersz3' ):
+    ?>
+  <div class="container-fluid px-5 my-5">
+      <?php the_sub_field('tekst3');?>
+</div>
+
+
+<?php
+    elseif( get_row_layout() == 'wiersz4' ):
+?>
+    <div class="container-fluid px-5 my-5">
+
+    <?php $image_url2 = get_sub_field('zdjecie3')['url']; ?>
+    <img class="services__image" src="<?php echo $image_url2 ?>" alt="">
+
+    </div>
+
+  <?php
+
+    endif;
+  endwhile;
+  else :
+  endif;
+
+?>
+
+
+<?php get_footer(); ?>
---
+++ page-service.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+  Template Name: Usługa
+*/
+
+get_header(); ?>
+
+
+<div class="container-fluid px-5 my-5">
+
+  <h1><?php the_title(); ?></h1>
+
+  <p class="service__row"><span class="service__desc"><i class="service__icon far fa-clock"></i> Czas trwania usługi: </span><?php the_field('czas_trwania_uslugi'); ?></p>
+  <p class="service__row"><span class="service__desc"><i class="service__icon far fa-user"></i> Osoba wykonująca usługę: </span><?php the_field('osoba_wykonujaca_usluge'); ?></p>
+
+  <div class="mt-4">
+  <?php while ( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+
+  <?php
+    endwhile;
+    wp_reset_query();
+  ?>
+  </div>
+
+</div>
+
+
+<?php
+    if( have_rows('uslugi') ):
+      ?>
+
+<?php
+      while ( have_rows('uslugi') ) : the_row();
+      if( get_row_layout() == 'wiersz1' ):
+        ?>
+
+<div class="container-fluid px-5 my-5">
+  <div class="row">
+    <div class="col">
+      <h2><?php the_sub_field('naglowek');?></h2>
+      <p><?php the_sub_field('tekst'); ?></p>
+    </div>
+
+    <div class="col">
+      <?php $image_url = get_sub_field('zdjecie')['url']; ?>
+      <img class="services__image" src="<?php echo $image_url ?>" alt="">
+    </div>
+  </div>
+
+</div>
+
+<?php
+    elseif( get_row_layout() == 'wiersz2' ):
+      ?>
+
+<div class="container-fluid px-5 my-5">
+  <div class="row">
+    <div class="col">
+      <?php $image_url = get_sub_field('zdjecie')['url']; ?>
+      <img class="services__image" src="<?php echo $image_url ?>" alt="">
+    </div>
+
+    <div class="col">
+      <h2><?php the_sub_field('naglowek');?></h2>
+      <p><?php the_sub_field('tekst'); ?></p>
+    </div>
+  </div>
+
+</div>
+
+<?php
+    elseif( get_row_layout() == 'wiersz3' ):
+    ?>
+  <div class="container-fluid px-5 my-5">
+    <h2><?php the_sub_field('naglowek');?></h2>
+    <p><?php the_sub_field('tekst'); ?></p>
+    <?php $image_url = get_sub_field('zdjecie')['url']; ?>
+    <img class="services__image" src="<?php echo $image_url ?>" alt="">
+</div>
+
+
+<div class="container-fluid px-5 my-5">
+<?php
+    elseif( get_row_layout() == 'wiersz4' ):
+?>
+<div class="container-fluid px-5 my-5">
+      <?php the_sub_field('tekst');?>
+</div>
+
+
+  <?php
+
+    endif;
+  endwhile;
+  else :
+  endif;
+
+?>
+</div>
+
+
+<?php get_footer(); ?>
